f070c1fff232b27147771ea28c9b5de0be7cddd6
ocsoares <casoares016@gmail.com>
2024-01-26 16:40:44 -0300

    feat: add AppEnvironmentVariables class to verify env. variables at startup

---
+++ src/main/java/com/ocsoares/awsemailsendingmicroservice/main/config/AppEnvironmentVariables.java
@@ -0,0 +1,18 @@
+package com.ocsoares.awsemailsendingmicroservice.main.config;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+// TENTAR mudar a Mensagem de ERRO quando as Variáveis de Env. NÃO EXISTEM !!!
+
+@Validated
+@Getter
+@Component
+public class AppEnvironmentVariables {
+    @NotBlank
+    @Value("${ONLY_TEST}")
+    private String onlyTest; // REMOVER ISSO DEPOIS, claro...
+}
