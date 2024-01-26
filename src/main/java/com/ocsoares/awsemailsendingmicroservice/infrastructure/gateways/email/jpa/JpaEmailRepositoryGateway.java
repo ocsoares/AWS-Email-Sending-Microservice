5ac28e1c1ea6655f172cb2482f61d897dbbe21e5
ocsoares <casoares016@gmail.com>
2024-01-26 17:49:38 -0300

    refactor: add RequiredArgsConstructor annotation

--- src/main/java/com/ocsoares/awsemailsendingmicroservice/infrastructure/gateways/email/jpa/JpaEmailRepositoryGateway.java
+++ src/main/java/com/ocsoares/awsemailsendingmicroservice/infrastructure/gateways/email/jpa/JpaEmailRepositoryGateway.java
@@ -4,17 +4,12 @@ import com.ocsoares.awsemailsendingmicroservice.application.gateways.email.IEmai
 import com.ocsoares.awsemailsendingmicroservice.domain.entity.EmailDomainEntity;
 import com.ocsoares.awsemailsendingmicroservice.infrastructure.mappers.EmailPersistenceEntityMapper;
 import com.ocsoares.awsemailsendingmicroservice.infrastructure.persistence.repository.jpa.JpaEmailRepository;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class JpaEmailRepositoryGateway implements IEmailRepositoryGateway {
     private final JpaEmailRepository jpaEmailRepository;
     private final EmailPersistenceEntityMapper emailPersistenceEntityMapper;
-
-    public JpaEmailRepositoryGateway(
-            JpaEmailRepository jpaEmailRepository, EmailPersistenceEntityMapper emailPersistenceEntityMapper
-    ) {
-        this.jpaEmailRepository = jpaEmailRepository;
-        this.emailPersistenceEntityMapper = emailPersistenceEntityMapper;
-    }
 
     @Override
     public EmailDomainEntity saveEmail(EmailDomainEntity emailDomainEntity) {
