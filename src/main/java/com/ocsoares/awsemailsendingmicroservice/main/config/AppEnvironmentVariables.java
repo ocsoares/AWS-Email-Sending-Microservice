52a39093bd380904033d3144ffd55b15aaefea66
ocsoares <casoares016@gmail.com>
2024-01-27 18:11:59 -0300

    chore: add AWS env. variables

--- src/main/java/com/ocsoares/awsemailsendingmicroservice/main/config/AppEnvironmentVariables.java
+++ src/main/java/com/ocsoares/awsemailsendingmicroservice/main/config/AppEnvironmentVariables.java
@@ -13,4 +13,16 @@ public class AppEnvironmentVariables {
     @NotBlank
     @Value("${EMAIL_SOURCE}")
     private String emailSource;
+
+    @NotBlank
+    @Value("${AWS_REGION}")
+    private String awsRegion;
+
+    @NotBlank
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String awsAccessKeyId;
+
+    @NotBlank
+    @Value("${AWS_SECRET_KEY}")
+    private String awsSecretKey;
 }
