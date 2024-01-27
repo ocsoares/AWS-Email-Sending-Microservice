0923195cd09dc47e490064e9e41ac3be828d3593
ocsoares <casoares016@gmail.com>
2024-01-27 18:46:09 -0300

    chore: remove comment

--- src/main/java/com/ocsoares/awsemailsendingmicroservice/application/usecases/response/EmailResponse.java
+++ src/main/java/com/ocsoares/awsemailsendingmicroservice/application/usecases/response/EmailResponse.java
@@ -1,6 +1,4 @@
 package com.ocsoares.awsemailsendingmicroservice.application.usecases.response;
 
-// Mudei os Parâmetros do User para o Email só para PARAR de dar Erro
-// OBS: Mas ainda NÃO SEI o EmailResponse q vou por...
 public record EmailResponse(String to) {
 }
--- src/main/java/com/ocsoares/awsemailsendingmicroservice/infrastructure/controllers/email/dtos/EmailDTO.java
+++ src/main/java/com/ocsoares/awsemailsendingmicroservice/infrastructure/controllers/email/dtos/EmailDTO.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-// Mudar esse Schema e os Parâmetros pq tá do User!!!
 @Schema(example = "{\"toRecipient\":\"johndoe@example.com\",\"subject\":\"Any subject\",\"body\":\"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\"}")
 public record EmailDTO(
         @NotBlank(message = "The toRecipient is required") @Email(message = "Must be a valid email address") String toRecipient,
